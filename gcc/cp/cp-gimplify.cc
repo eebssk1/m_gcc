gcc-mirror/gcc

c++: avoid ARM -Wunused-value [PR114970]
Because of the __builtin_is_constant_evaluated, maybe_constant_init in
expand_default_init fails, so the constexpr constructor isn't folded until
cp_fold, which then calls cp_build_init_expr_for_ctor, which builds a
COMPOUND_EXPR in case the enclosing expression is relying on the ARM
behavior of returning 'this'.

As in other places, avoid -Wunused-value on artificial COMPOUND_EXPR.

	PR c++/114970

gcc/cp/ChangeLog:

	* cp-gimplify.cc (cp_build_init_expr_for_ctor): Suppress warnings on
	return_this COMPOUND_EXPR.

gcc/testsuite/ChangeLog:

	* g++.dg/opt/is_constant_evaluated4.C: New test.

gcc/cp/cp-gimplify.cc
@@ -1199,8 +1199,11 @@ cp_build_init_expr_for_ctor (tree call, tree init)
   tree s = build_fold_indirect_ref_loc (loc, a);
   init = cp_build_init_expr (s, init);
   if (return_this)
-    init = build2_loc (loc, COMPOUND_EXPR, TREE_TYPE (call), init,
-		    fold_convert_loc (loc, TREE_TYPE (call), a));
+    {
+      init = build2_loc (loc, COMPOUND_EXPR, TREE_TYPE (call), init,
+			 fold_convert_loc (loc, TREE_TYPE (call), a));
+      suppress_warning (init);
+    }
   return init;
 }
 

eebssk1/m_gcc

cp_fold, which builds a COMPOUND_EXPR in case the enclosing expression is
relying on the ARM behavior of returning 'this'.
	* cp-gimplify.cc (cp_fold): Suppress warnings on
(cherry picked from commit 4acdfb71d4fdaa43c2707ad7b2fb7b2b7bddfc42)

gcc/cp/cp-gimplify.cc
@@ -3437,8 +3437,11 @@ cp_fold (tree x, fold_flags_t flags)
 		tree s = build_fold_indirect_ref_loc (loc, a);
 		r = cp_build_init_expr (s, r);
 		if (return_this)
-		  r = build2_loc (loc, COMPOUND_EXPR, TREE_TYPE (x), r,
-				  fold_convert_loc (loc, TREE_TYPE (x), a));
+		  {
+		    r = build2_loc (loc, COMPOUND_EXPR, TREE_TYPE (x), r,
+				    fold_convert_loc (loc, TREE_TYPE (x), a));
+		    suppress_warning (r);
+		  }
 	      }
 	    x = r;
 	    break;