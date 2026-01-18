gcc-mirror/gcc

dwarf: Save bit stride information for array type entry [PR121964]
Lack of DW_AT_bit_stride in a DW_TAG_array_type entry causes GDB to infer
incorrect element size for vector types. The causes incorrect display of
SVE predicate variables as well as out of bounds memory access when reading
contents of SVE predicates from memory in GDB.

We also locate DIE referenced by DW_AT_type and set DW_AT_bit_size 1 in it.

	PR debug/121964

gcc/
	* dwarf2out.cc (gen_array_type_die): Add DW_AT_bit_stride attribute
	for array types based on element type bit precision for integer and
	boolean element types.

gcc/testsuite/
	* g++.target/aarch64/dwarf-bit-stride-func.C: New test.
	* g++.target/aarch64/dwarf-bit-stride-pragma.C: New test.
	* g++.target/aarch64/dwarf-bit-stride-pragma-sme.C: New test.
	* g++.target/aarch64/sve/dwarf-bit-stride.C: New test.
	* gcc.target/aarch64/dwarf-bit-stride-func.c: New test.
	* gcc.target/aarch64/dwarf-bit-stride-pragma.c: New test.
	* gcc.target/aarch64/dwarf-bit-stride-pragma-sme.c: New test.
	* gcc.target/aarch64/sve/dwarf-bit-stride.c: New test.

gcc/testsuite/g++.target/aarch64/dwarf-bit-stride-pragma-sme.C
@@ -1,0 +1,16 @@
+/* { dg-do compile } */
+// { dg-options "-g -dA" }
+// { dg-final { scan-assembler-times "DW_AT_name: \"svbool_t\"" 1 } }
+// { dg-final { scan-assembler-times "DW_AT_name: \"svcount_t\"" 1 } }
+// { dg-final { scan-assembler-times ".byte	0x1	// DW_AT_bit_stride" 1 } }
+// { dg-final { scan-assembler-times ".byte	0x1	// DW_AT_bit_size" 1 } }
+
+#include <arm_sme.h>
+
+#pragma GCC target "+sve2p1+sme2"
+
+void fun ()
+{
+  volatile svbool_t pred;
+  volatile svcount_t count;
+}

gcc/testsuite/gcc.target/aarch64/dwarf-bit-stride-pragma-sme.c
@@ -1,0 +1,14 @@
+/* { dg-do compile } */
+// { dg-options "-g -dA" }
+// { dg-final { scan-assembler-times "DW_AT_name: \"__SVBool_t\"" 2 } }
+// { dg-final { scan-assembler-times "DW_AT_name: \"__SVCount_t\"" 1 } }
+// { dg-final { scan-assembler-times ".byte	0x1	// DW_AT_bit_stride" 2 } }
+// { dg-final { scan-assembler-times ".byte	0x1	// DW_AT_bit_size" 1 } }
+
+#pragma GCC target "+sve2p1+sme2"
+
+void fun ()
+{
+  volatile __SVBool_t pred;
+  volatile __SVCount_t count;
+}

eebssk1/m_gcc

Minor tweak in target flags in tests compared to GCC-15 version.
(cherry picked from commit ba39d22f0b3f01195d8164649fc025653827f6b6)

gcc/testsuite/g++.target/aarch64/dwarf-bit-stride-pragma-sme.C
@@ -1,0 +1,16 @@
+/* { dg-do compile } */
+// { dg-options "-g -dA" }
+// { dg-final { scan-assembler-times "DW_AT_name: \"svbool_t\"" 1 } }
+// { dg-final { scan-assembler-times "DW_AT_name: \"svcount_t\"" 1 } }
+// { dg-final { scan-assembler-times ".byte	0x1	// DW_AT_bit_stride" 1 } }
+// { dg-final { scan-assembler-times ".byte	0x1	// DW_AT_bit_size" 1 } }
+
+#include <arm_sme.h>
+
+#pragma GCC target "+sve2+sme2"
+
+void fun ()
+{
+  volatile svbool_t pred;
+  volatile svcount_t count;
+}

gcc/testsuite/gcc.target/aarch64/dwarf-bit-stride-pragma-sme.c
@@ -1,0 +1,14 @@
+/* { dg-do compile } */
+// { dg-options "-g -dA" }
+// { dg-final { scan-assembler-times "DW_AT_name: \"__SVBool_t\"" 2 } }
+// { dg-final { scan-assembler-times "DW_AT_name: \"__SVCount_t\"" 1 } }
+// { dg-final { scan-assembler-times ".byte	0x1	// DW_AT_bit_stride" 2 } }
+// { dg-final { scan-assembler-times ".byte	0x1	// DW_AT_bit_size" 1 } }
+
+#pragma GCC target "+sve2+sme2"
+
+void fun ()
+{
+  volatile __SVBool_t pred;
+  volatile __SVCount_t count;
+}