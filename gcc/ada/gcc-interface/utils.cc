gcc-mirror/gcc

Ada: Fix ICE in fld_incomplete_type_of when building GtkAda with LTO (3)
This streamlines the code by using a ternary expression.

gcc/ada/
	PR ada/123060
	* gcc-interface/utils.cc (update_pointer_to): Streamline.

gcc/ada/gcc-interface/utils.cc
@@ -4689,11 +4689,10 @@ update_pointer_to (tree old_type, tree new_type)
 		       && !lookup_attribute ("may_alias",
 					     TYPE_ATTRIBUTES (new_type))))
 	    {
-	      alias_set_type set;
-	      if (TYPE_STRUCTURAL_EQUALITY_P (old_ptr))
-		set = TYPE_ALIAS_SET (old_ptr);
-	      else
-		set = TYPE_ALIAS_SET (TYPE_CANONICAL (old_ptr));
+	      const alias_set_type set
+		= TYPE_STRUCTURAL_EQUALITY_P (old_ptr)
+		  ? TYPE_ALIAS_SET (old_ptr)
+		  : TYPE_ALIAS_SET (TYPE_CANONICAL (old_ptr));
 	      TYPE_CANONICAL (old_ptr)
 		= build_pointer_type (TYPE_CANONICAL (new_type));
 	      TYPE_ALIAS_SET (TYPE_CANONICAL (old_ptr)) = set;
@@ -4734,11 +4733,10 @@ update_pointer_to (tree old_type, tree new_type)
 		       && !lookup_attribute ("may_alias",
 					     TYPE_ATTRIBUTES (new_type))))
 	    {
-	      alias_set_type set;
-	      if (TYPE_STRUCTURAL_EQUALITY_P (old_ref))
-		set = TYPE_ALIAS_SET (old_ref);
-	      else
-		set = TYPE_ALIAS_SET (TYPE_CANONICAL (old_ref));
+	      const alias_set_type set
+		= TYPE_STRUCTURAL_EQUALITY_P (old_ref)
+		  ? TYPE_ALIAS_SET (old_ref)
+		  : TYPE_ALIAS_SET (TYPE_CANONICAL (old_ref));
 	      TYPE_CANONICAL (old_ref)
 		= build_reference_type (TYPE_CANONICAL (new_type));
 	      TYPE_ALIAS_SET (TYPE_CANONICAL (old_ref)) = set;

eebssk1/m_gcc

gcc/ada/gcc-interface/utils.cc
@@ -4469,11 +4469,10 @@ update_pointer_to (tree old_type, tree new_type)
 		       && !lookup_attribute ("may_alias",
 					     TYPE_ATTRIBUTES (new_type))))
 	    {
-	      alias_set_type set;
-	      if (TYPE_STRUCTURAL_EQUALITY_P (ptr))
-		set = TYPE_ALIAS_SET (ptr);
-	      else
-		set = TYPE_ALIAS_SET (TYPE_CANONICAL (ptr));
+	      const alias_set_type set
+		= TYPE_STRUCTURAL_EQUALITY_P (ptr)
+		  ? TYPE_ALIAS_SET (ptr)
+		  : TYPE_ALIAS_SET (TYPE_CANONICAL (ptr));
 	      TYPE_CANONICAL (ptr)
 		= build_pointer_type (TYPE_CANONICAL (new_type));
 	      TYPE_ALIAS_SET (TYPE_CANONICAL (ptr)) = set;
@@ -4514,11 +4513,10 @@ update_pointer_to (tree old_type, tree new_type)
 		       && !lookup_attribute ("may_alias",
 					     TYPE_ATTRIBUTES (new_type))))
 	    {
-	      alias_set_type set;
-	      if (TYPE_STRUCTURAL_EQUALITY_P (ref))
-		set = TYPE_ALIAS_SET (ref);
-	      else
-		set = TYPE_ALIAS_SET (TYPE_CANONICAL (ref));
+	      const alias_set_type set
+		= TYPE_STRUCTURAL_EQUALITY_P (ref)
+		  ? TYPE_ALIAS_SET (ref)
+		  : TYPE_ALIAS_SET (TYPE_CANONICAL (ref));
 	      TYPE_CANONICAL (ref)
 		= build_reference_type (TYPE_CANONICAL (new_type));
 	      TYPE_ALIAS_SET (TYPE_CANONICAL (ref)) = set;