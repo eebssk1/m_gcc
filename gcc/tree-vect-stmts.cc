gcc-mirror/gcc

tree-optimization/123741 - fix segfault with BB vect and masked stores
When vectorizing the body of an if-converted loop with BB vectorization
we can end up vectorizing masked stores.  But the code tracking whether
a loop used masked stores failed to check we're doing loop
vectorization.

	PR tree-optimization/123741
	* tree-vect-stmts.cc (vectorizable_store): Add missing check
	on loop vectorization.

	* gfortran.dg/vect/vect-pr123741.f90: New testcase.

gcc/tree-vect-stmts.cc
@@ -8694,7 +8694,7 @@ vectorizable_store (vec_info *vinfo,
 					    memory_access_type, loop_lens);
     }
 
-  if (mask_node && !costing_p)
+  if (loop_vinfo && mask_node && !costing_p)
     LOOP_VINFO_HAS_MASK_STORE (loop_vinfo) = true;
 
   /* In case the vectorization factor (VF) is bigger than the number

eebssk1/m_gcc

(cherry picked from commit c7c5112fa19eeb01656e000c2b7732e86be8782e)

gcc/tree-vect-stmts.cc
@@ -8766,7 +8766,7 @@ vectorizable_store (vec_info *vinfo,
 					  memory_access_type, loop_lens);
     }
 
-  if (mask && !costing_p)
+  if (loop_vinfo && mask && !costing_p)
     LOOP_VINFO_HAS_MASK_STORE (loop_vinfo) = true;
 
   /* In case the vectorization factor (VF) is bigger than the number